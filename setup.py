976ce62088eda9a39cb47fc3ab5a8103fe729a15
Alex Reich <alex@alexreich.com>
2019-10-26 21:23:55 -0700

    + version for PyPI

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 setuptools.setup(
      name='watermoccasin',  
-     version='1.0',
+     version='1.01',
      author="Alex Reich",
      author_email="watermoccasin@alexreich.com",
      description="Convert a NPR ONE news feed for offline listening with options for speed up and timed playlist",
